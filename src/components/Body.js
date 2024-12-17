b6fb83e2b0cd3a652fbc632067d97bf816a2e8c8
bharathsampangi <bharathsampangi95@gmail.com>
2024-12-17 14:37:53 +0530

    shimmer

---
+++ src/components/Body.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import MemeCard from "./MemeCard"
+import Shimmer from "./Shimmer"
+
+const Body = () => {
+
+    const [memes, setMemes] = useState(null)
+
+    useEffect(() => {
+        fetchMemes()
+    }, [])
+
+    const fetchMemes = async () => {
+        const data = await fetch("https://meme-api.com/gimme/20")
+        const json = await data.json()
+        setMemes(json.memes)
+    }
+
+    return (
+        <div className="flex flex-wrap">
+            {!memes ? <Shimmer /> :
+            memes.map((meme, i) => <MemeCard data={meme} key={i} />)}
+        </div>
+    )
+}
+
+export default Body
---
+++ src/components/MemeCard.js
@@ -0,0 +1,13 @@
+const MemeCard = ({data}) => {
+
+    const {url, title, author} = data
+
+    return (
+        <div className="p-5 m-5 border border-black rounded-lg">
+            <img className="w-64 h-64" alt={title} src={url} />
+            <p>{author}</p>
+        </div>
+    )
+}
+
+export default MemeCard
